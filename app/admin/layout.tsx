8d67f26d113113365b79e8afb70949027801f713
fohalabi <ayomideabimbola79@gmail.com>
2026-01-02 21:30:50 +0100

    feat: updated the stock sync page to fectch real time stock number as orders is been made

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,21 +1,30 @@
+'use client';
+
+import { useEffect } from 'react';
 import ProtectedRoute from '@/components/ProtectedRoutes';
 import { Sidebar } from '@/components/admin/sidebar/Sidebar';
+import { initializeSocket, disconnectSocket } from '@/lib/socket';
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    initializeSocket();
+    return () => disconnectSocket();
+  }, []);
+
   return (
     <ProtectedRoute allowedRoles={['ADMIN', 'MANAGER', 'SUPPORT']}>
       <div className="h-screen flex overflow-hidden">
-      <aside className="overflow-y-auto">
-        <Sidebar />
-      </aside>
-      <main className="flex-1 overflow-y-auto bg-gray-200 dark:bg-gray-900 pt-16 lg:pt-0">
-        {children}
-      </main>
-    </div>
+        <aside className="overflow-y-auto">
+          <Sidebar />
+        </aside>
+        <main className="flex-1 overflow-y-auto bg-gray-200 dark:bg-gray-900 pt-16 lg:pt-0">
+          {children}
+        </main>
+      </div>
     </ProtectedRoute>
   );
 }
